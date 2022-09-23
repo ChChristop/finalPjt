b5be8b1dd0850139b2f4af76e528a63cf78018e5
ChChristop <105895551+ChChristop@users.noreply.github.com>
2022-09-23 16:55:19 +0900

    Google Login

--- modals/SignInModal.js
+++ modals/SignInModal.js
@@ -1,36 +1,38 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Modal, Button, Form, Container } from "react-bootstrap";
-import HorizonLine from "../component/HorizonLine";
-import InputGroup from "react-bootstrap/InputGroup";
-import GoogleLogin from "react-google-login";
-import { login } from "./userSlice";
-import { useDispatch } from "react-redux";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Modal, Button, Form, Container } from 'react-bootstrap';
+import HorizonLine from '../component/HorizonLine';
+import InputGroup from 'react-bootstrap/InputGroup';
+import GoogleLogin from 'react-google-login';
+import { login } from './userSlice';
+import { useDispatch } from 'react-redux';
+
 
 // 로그인
-const SignInModal = ({ show, onHide, test }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const SignInModal = ({ show, onHide }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [adminCheck, setCheck] = useState(true);
-  // const dispach = useDispatch();
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
+  const dispach = useDispatch();
 
-  //   dispach(
-  //     login({
-  //       memberID: email,
-  //       memberPW: password,
-  //       checkbox: abc,
-  //       loggdeIn: true,
-  //     })
-  //   );
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispach(
+      login({
+        memberID: email,
+        memberPW: password,
+        //checkbox: abc,
+        loggdeIn: true,
+      })
+    );
+  };
 
   const mamber = () => {
-    let adminURL = "http://192.168.0.10:8080/api/login/admin";
-    let memberURL = "http://192.168.0.10:8080/api/login/member";
-    let url = "";
+    let adminURL = 'http://192.168.0.10:8080/api/login/admin';
+    let memberURL = 'http://192.168.0.10:8080/api/login/member';
+    let url = '';
     let abc = !adminCheck;
 
     if (abc == true) {
@@ -38,7 +40,7 @@ const SignInModal = ({ show, onHide, test }) => {
     } else {
       url = memberURL;
     }
-    console.log("check:" + abc);
+    console.log('check:' + abc);
     axios
       .post(url, {
         memberID: email,
@@ -54,23 +56,42 @@ const SignInModal = ({ show, onHide, test }) => {
           refreshtoken: response.headers.refreshtoken,
         };
         // Handle success.
-        console.log("Well done!");
-        console.log("User profile", user);
+        console.log('Well done!');
+        console.log('User profile', user);
         // console.log("User token", response.data.jwt);
-        console.log(response.headers["authorization"]);
-        console.log(response.headers["refreshtoken"]);
-        alert("환영합니다");
-        test(true);
-        setEmail("");
-        setPassword("");
+        console.log(response.headers['authorization']);
+        console.log(response.headers['refreshtoken']);
+        alert('환영합니다');
+        setEmail('');
+        setPassword('');
       })
       .catch((error) => {
         // Handle error.
-        console.log("An error occurred:", error.response);
-        alert("로그인 실패하였습니다");
-        setEmail("");
-        setPassword("");
+        console.log('An error occurred:', error.response);
+        alert('로그인 실패하였습니다');
+        setEmail('');
+        setPassword('');
       });
+  };
+
+  const onSuccess = async (response) => {
+    const {
+      googleId,
+      profileObj: { email, name },
+    } = response;
+
+    console.log(response);
+    // await onSocial({
+    //   socialId: googleId,
+    //   socialType: 'google',
+    //   email,
+    //   nickname: name,
+    // });
+  };
+
+  const onFailure = (error) => {
+    console.log(error);
+    console.log(error.details);
   };
 
   return (
@@ -109,7 +130,6 @@ const SignInModal = ({ show, onHide, test }) => {
                 placeholder="비밀번호를 입력해주세요"
               />
             </Form.Group>
-
             <Button
               variant="info"
               type="button"
@@ -132,7 +152,13 @@ const SignInModal = ({ show, onHide, test }) => {
             />
             <span className="checkboxContent">관리자</span>
           </InputGroup>
-          <HorizonLine text={"OR"} />
+          <HorizonLine text={'OR'} />
+          <GoogleLogin
+            clientId={clientId}
+            responseType={'id_token'}
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+          />
         </Modal.Body>
       </Container>
     </Modal>
